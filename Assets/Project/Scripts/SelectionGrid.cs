f0c164a896d9ddf3603fb4f994f4d3df8434ac15
Taro Omiya <taro.omiya@omiyagames.com>
2018-08-11 10:21:27 -0400

    Working on SelectionGrid.

---
+++ Assets/Project/Scripts/SelectionGrid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project
+{
+    public class SelectionGrid : MonoBehaviour
+    {
+        [SerializeField]
+        BlockGrid grid;
+
+        [Header("UI")]
+        [SerializeField]
+        Canvas canvas;
+        [SerializeField]
+        LayoutGroup layout;
+        [SerializeField]
+        GameObject selector;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
