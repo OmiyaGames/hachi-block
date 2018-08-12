a3b4da19cc24fea3238e13b3fb20792cfbb339d1
Taro Omiya <taro.omiya@omiyagames.com>
2018-08-11 22:09:01 -0400

    Adding placement animation.  Fixing the background deselecting on rotate.

---
+++ Assets/Project/Scripts/BlockCursorDeselector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Project
+{
+    public class BlockCursorDeselector : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField]
+        BlockCursor cursor;
+
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                cursor.Deselect();
+            }
+        }
+    }
+}
